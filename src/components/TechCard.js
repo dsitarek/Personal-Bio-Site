e46a5c290e31cd6ea4365aff3dae496c665d06f0
Daniel Sitarek <deltasierra777@gmail.com>
2021-11-16 21:21:19 -0600

    tech-card update for image and style

--- src/components/TechCard.js
+++ src/components/TechCard.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 
 export default function TechCard({ tech }) {
   return (
-    <div>
+    <div className="tech-card">
+      <img src={tech.imageURL} className="tech-image" alt={tech.name} />
       {tech.name}
     </div>
   );
@@ -12,5 +13,6 @@ export default function TechCard({ tech }) {
 TechCard.propTypes = {
   tech: PropTypes.shape({
     name: PropTypes.string,
+    imageURL: PropTypes.string,
   }).isRequired,
 };
